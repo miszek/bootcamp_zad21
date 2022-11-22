4e40374a36ed597ae42229b9d942891259a0a592
Michal Szekalski <szekalski.michal@gmail.com>
2022-11-22 23:53:12 +0100

    problem z dodawaniem produktów

---
+++ src/main/java/com/michalszekalski/bootcamp_zad21/ProductsRepo.java
@@ -0,0 +1,48 @@
+package com.michalszekalski.bootcamp_zad21;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductsRepo {
+    private List<Product> products = new ArrayList<>();
+
+    public ProductsRepo() {
+        products.add(new Product("chleb", 4.99, Category.FOOD));
+        products.add(new Product("ocet", 7.50, Category.FOOD));
+        products.add(new Product("pralka", 1399.00, Category.HOUSEHOLD));
+        products.add(new Product("Zelazko", 129.99, Category.HOUSEHOLD));
+        products.add(new Product("książka", 44.99, Category.OTHER));
+        products.add(new Product("rower", 2600.00, Category.OTHER));
+        products.add(new Product("sanki", 200.00, Category.OTHER));
+    }
+
+    public List<Product> getAll () {
+        return products;
+    }
+
+    public List<Product> findByCategory(Category category) {
+        return products.stream()
+                .filter(p -> p.getCategory().equals(category))
+                .toList();
+    }
+
+    public double pricesSumByCategory(Category category) {
+        return products.stream()
+                .filter(p -> p.getCategory().equals(category))
+                .map(p -> p.getPrice())
+                .reduce(0D, (subtotal, p) -> subtotal + p);
+    }
+
+    public double pricesSumAllProducts() {
+        return products.stream()
+                .map(p -> p.getPrice())
+                .reduce(0D, (subtotal, p) -> subtotal + p);
+    }
+
+    public void add(Product product) {
+        products.add(product);
+    }
+}
