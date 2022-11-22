e5e57419dbac90b530d2d464213a7ca859b79c97
Michal Szekalski <szekalski.michal@gmail.com>
2022-11-22 21:45:26 +0100

    new project

---
+++ src/main/java/com/michalszekalski/bootcamp_zad21/BootcampZad21Application.java
@@ -0,0 +1,13 @@
+package com.michalszekalski.bootcamp_zad21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampZad21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampZad21Application.class, args);
+    }
+
+}
